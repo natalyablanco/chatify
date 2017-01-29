33b665dfadc29132468e82fbbcfa4705fbc2bbc2
Natalya Blanco <blanco@ubilabs.net>
2017-01-29 16:21:08 +0100

    First commit. Installing dependencies, understanding concepts

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+  apiKey: "AIzaSyAfthK7fImEgclUeh5gn0Bo4JgROuTBtFY",
+  authDomain: "blistering-heat-3128.firebaseapp.com",
+  databaseURL: "https://blistering-heat-3128.firebaseio.com",
+  storageBucket: "blistering-heat-3128.appspot.com",
+  messagingSenderId: "255293020454"
+};
+firebase.initializeApp(config);
